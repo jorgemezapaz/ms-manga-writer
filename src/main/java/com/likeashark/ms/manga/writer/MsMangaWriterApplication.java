9d94ba13e1d7bf519cdec2e9b86bf00b7a34b96d
jorgemezapaz <jorge.meza.paz@gmail.com>
2021-08-10 23:35:56 -0400

    base structure
    ms init

---
+++ src/main/java/com/likeashark/ms/manga/writer/MsMangaWriterApplication.java
@@ -0,0 +1,13 @@
+package com.likeashark.ms.manga.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMangaWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMangaWriterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likeashark/ms/manga/writer/MsMangaWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likeashark.ms.manga.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMangaWriterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
